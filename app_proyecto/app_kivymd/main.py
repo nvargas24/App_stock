fcce22f3c41f13f80e8d4e4b3b1852a6a129b776
DiegoC <diegoacalderon994@gmail.com>
2023-06-28 18:18:49 -0300

    Modificación de main.py y main.kv para cambio entre pantallas. Agregado de archivos agregar, eliminar y modificar.

--- app_proyecto/app_kivymd/main.py
+++ app_proyecto/app_kivymd/main.py
@@ -1,9 +1,23 @@
 from kivy.metrics import dp
+from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.uix.screenmanager import MDScreenManager
 from kivymd.uix.menu import MDDropdownMenu
 from kivymd.uix.list import OneLineIconListItem
+from kivymd.uix.boxlayout import MDBoxLayout
 from kivy.properties import StringProperty
+
+
+class Agregar(MDBoxLayout):
+    Builder.load_file("agregar.kv")
+
+
+class Eliminar(MDBoxLayout):
+    Builder.load_file("eliminar.kv")
+
+
+class Modificar(MDBoxLayout):
+    Builder.load_file("modificar.kv")
 
 
 class IconListItem(OneLineIconListItem):
@@ -14,30 +28,7 @@ class MisPantallas(MDScreenManager):
     def __init__(self, app, **kwargs):
         super(MisPantallas, self).__init__(**kwargs)
         self.app = app
-        self.menu_items = [
-            {
-                "icon": "weather-sunny",
-                "text": "Tema Claro",
-                "theme_text_color": "Custom",
-                "text_color": self.app.theme_cls.opposite_bg_darkest,
-                "viewclass": "IconListItem",
-                "on_release": lambda x="claro": self.cambiar_tema(x),
-            },
-            {
-                "icon": "moon-waning-crescent",
-                "text": "Tema Oscuro",
-                "theme_text_color": "Custom",
-                "text_color": self.app.theme_cls.opposite_bg_darkest,
-                "viewclass": "IconListItem",
-                "on_release": lambda x="oscuro": self.cambiar_tema(x),
-            },
-        ]
-        self.menu = MDDropdownMenu(
-            caller=self.ids.button,
-            items=self.menu_items,
-            max_height=100,
-            width_mult=3,
-        )
+        self.crear_menu()
 
     def cambiar_tema(self, value):
         if value == "claro":
@@ -48,7 +39,7 @@ class MisPantallas(MDScreenManager):
             self.ids.buttondel.md_bg_color = "#C7C7C7"
             self.ids.buttonedit.md_bg_color = "#C7C7C7"
             self.ids.buttonsearch.md_bg_color = "#C7C7C7"
-            self.update_menu_text_color()
+            self.crear_menu()
         else:
             self.menu.dismiss()
             self.app.theme_cls.primary_palette = "Orange"
@@ -57,9 +48,9 @@ class MisPantallas(MDScreenManager):
             self.ids.buttondel.md_bg_color = "#404040"
             self.ids.buttonedit.md_bg_color = "#404040"
             self.ids.buttonsearch.md_bg_color = "#404040"
-            self.update_menu_text_color()
+            self.crear_menu()
 
-    def update_menu_text_color(self):
+    def crear_menu(self):
         self.menu_items = [
             {
                 "icon": "weather-sunny",
@@ -81,8 +72,8 @@ class MisPantallas(MDScreenManager):
         self.menu = MDDropdownMenu(
             caller=self.ids.button,
             items=self.menu_items,
-            max_height=100,
-            width_mult=3,
+            max_height=dp(100),
+            width_mult=dp(3),
         )
 
 
